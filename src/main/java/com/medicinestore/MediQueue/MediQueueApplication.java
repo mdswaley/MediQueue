b6be59d9f103c699dd880c7fbd23b77ff2f6847a
mdswaley <mdswaley075@gmail.com>
2026-07-21 15:22:07 +0530

    first commit

---
+++ src/main/java/com/medicinestore/MediQueue/MediQueueApplication.java
@@ -0,0 +1,13 @@
+package com.medicinestore.MediQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicinestore/MediQueue/MediQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicinestore.MediQueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
