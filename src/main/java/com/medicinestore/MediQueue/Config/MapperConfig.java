4970cc27dd098c743eb72026c73e7ebaa66fb485
mdswaley <mdswaley075@gmail.com>
2026-07-22 19:18:36 +0530

    Add modelMapper

--- src/main/java/com/medicinestore/MediQueue/Config/MapperConfig.java
+++ src/main/java/com/medicinestore/MediQueue/Config/MapperConfig.java
@@ -1,5 +1,6 @@
 package com.medicinestore.MediQueue.Config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfig {
     @Bean
     public ModelMapper mapper(){
-        return new ModelMaper();
+        return new ModelMapper();
     }
 }
